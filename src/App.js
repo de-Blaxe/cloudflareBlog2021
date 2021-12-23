c48cd8484291cdab2202e1d9dda3c8dc0bba10ec
Donald Lieu <dclieu@ucdavis.edu>
2021-12-23 14:13:42 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Router } from "@reach/router";
+
+import Posts from './components/posts'
+import Post from './components/post'
+
+function App() {
+  return (
+    <div>
+      <span>Hello world</span>
+    </div>
+  );
+}
+
+export default App;
